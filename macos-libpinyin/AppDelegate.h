9bfadc0c9599f514df47dcde90d266b03b318d55
Inoki <veyx.shaw@gmail.com>
2021-03-06 14:50:43 +0100

    Init project
    Add data/table.conf
    Add codes

---
+++ macos-libpinyin/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  macos-libpinyin
+//
+//  Created by inoki on 3/6/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
