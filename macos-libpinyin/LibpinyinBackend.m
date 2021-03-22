f124f6979e189fcc4b1c3b1d054d96b15af86ddc
Inoki <veyx.shaw@gmail.com>
2021-03-22 19:03:03 +0100

    Allow to use fuzzy pinyin and incomplete pinyin

--- macos-libpinyin/LibpinyinBackend.m
+++ macos-libpinyin/LibpinyinBackend.m
@@ -7,6 +7,8 @@
 
 #import "LibpinyinBackend.h"
 
+#import "LibpinyinConfig.h"
+
 static id _instance;
 
 @implementation LibpinyinBackend {
@@ -28,10 +30,11 @@ static id _instance;
 }
 
 - (pinyin_instance_t *)allocPinyinInstance {
+    LibpinyinConfig *config = [LibpinyinConfig sharedConfig];
     if (nil == m_pinyin_context) {
         m_pinyin_context = [self initPinyinContext];
     }
-    // TODO: Set pinyin options
+    [self setPinyinOptions:config];
     return pinyin_alloc_instance (m_pinyin_context);
 }
 
@@ -52,5 +55,14 @@ static id _instance;
     return context;
 }
 
+- (BOOL)setPinyinOptions:(LibpinyinConfig *)config {
+    if (m_pinyin_context == NULL) return NO;
+
+    DoublePinyinScheme scheme = [config doublePinyinSchema];
+    pinyin_set_double_pinyin_scheme (m_pinyin_context, scheme);
+    pinyin_option_t options = (pinyin_option_t)([config option] | USE_RESPLIT_TABLE | USE_DIVIDED_TABLE);
+    pinyin_set_options (m_pinyin_context, options);
+    return YES;
+}
 
 @end
