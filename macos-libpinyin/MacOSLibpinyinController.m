0f128b9d8e4871895c29d90be3f4457a3be21033
Inoki <veyx.shaw@gmail.com>
2021-03-23 10:59:50 +0100

    Add multiple editors for modes in controller

--- macos-libpinyin/MacOSLibpinyinController.m
+++ macos-libpinyin/MacOSLibpinyinController.m
@@ -9,11 +9,24 @@
 #import "LibpinyinFullPinyinEditor.h"
 #import "AppDelegate.h"
 
+enum EditorMode {
+    PINYIN_EDITOR = 0,
+    PUNCT_EDITOR,
+    RAW_EDITOR,
+    ENGLISH_EDITOR,
+};
+
 @implementation MacOSLibpinyinController {
+    enum EditorMode mode;
+    NSMutableArray *editors;
     LibpinyinFullPinyinEditor *_fullpinyinEditor;
     BOOL chineseMode;
     BOOL traditionMode;
     BOOL fullPunct;
+
+    // Language switch
+    BOOL hasInputBetweenShift;
+    BOOL shiftPressed;
 }
 
 - (BOOL)inputText:(NSString*)string client:(id)sender
@@ -24,8 +37,8 @@
 
 - (BOOL)handleEvent:(NSEvent *)event client:(id)sender
 {
-    if (_fullpinyinEditor == nil) {
-        NSLog(@"No editor can handle this");
+    if (![editors count]) {
+        NSLog(@"No editor can handle this %lu", [editors count]);
         return NO;
     }
     LibpinyinConfig *config = [LibpinyinConfig sharedConfig];
@@ -36,21 +49,75 @@
     int keyCode;
     int keyValue;
     BOOL handled = NO;
-    switch ([event type]) {
-        case NSEventTypeFlagsChanged:
-            // TODO: modifier
-            NSLog(@"Flags changed: %@", event);
-            break;
-        case NSEventTypeKeyDown:
-            // Key down: send key code and value
-            NSLog(@"%hu %@ %@", [event keyCode], [event charactersIgnoringModifiers], [event characters]);
-            keyCode = [event keyCode];
-            keyValue = [[event characters] UTF8String][0];  // TODO: use unmodified?
-            handled = [_fullpinyinEditor processKeyEventWithKeyValue:keyValue keyCode:keyCode modifiers:0];
-            [_fullpinyinEditor refresh:sender underController:self];
-            break;
-        default:
-            break;
+    if (chineseMode) {
+        // Handle with Pinyin or other editors
+        NSUInteger currentEditorIndex;
+        switch ([event type]) {
+            case NSEventTypeFlagsChanged:
+                // Mode switch checking
+                if (shiftPressed && !hasInputBetweenShift) {
+                    if (!([event modifierFlags] & NSEventModifierFlagShift)) {
+                        // Shift Released without key, switch mode
+                        shiftPressed = NO;
+                        [config setInitChinese:NO];
+                        chineseMode = NO;
+                    }
+                }
+                if ([event modifierFlags] & NSEventModifierFlagShift) {
+                    // Pressed, begin checking key event
+                    shiftPressed = YES;
+                    hasInputBetweenShift = NO;
+                }
+                // TODO: modifier
+                NSLog(@"Flags changed: %@", event);
+                break;
+            case NSEventTypeKeyDown:
+                if (shiftPressed) {
+                    // Note the event
+                    hasInputBetweenShift = YES;
+                }
+                currentEditorIndex = (NSUInteger)mode;
+                // Key down: send key code and value
+                NSLog(@"%hu %@ %@", [event keyCode], [event charactersIgnoringModifiers], [event characters]);
+                keyCode = [event keyCode];
+                keyValue = [[event characters] UTF8String][0];  // TODO: use unmodified?
+                handled = [[editors objectAtIndex:currentEditorIndex] processKeyEventWithKeyValue:keyValue keyCode:keyCode modifiers:0];
+                [[editors objectAtIndex:currentEditorIndex] refresh:sender underController:self];
+                break;
+            default:
+                break;
+        }
+    } else {
+        // Full English mode
+        // Do not handle key event, only wait <Shift> to swtich back to chinese mode
+        switch ([event type]) {
+            case NSEventTypeFlagsChanged:
+                // Mode switch checking
+                if (shiftPressed && !hasInputBetweenShift) {
+                    if (!([event modifierFlags] & NSEventModifierFlagShift)) {
+                        // Shift Released without key, switch mode
+                        shiftPressed = NO;
+                        [config setInitChinese:YES];
+                        chineseMode = YES;
+                    }
+                }
+                if ([event modifierFlags] & NSEventModifierFlagShift) {
+                    // Pressed, begin checking key event
+                    shiftPressed = YES;
+                    hasInputBetweenShift = NO;
+                }
+                break;
+            case NSEventTypeKeyDown:
+                if (shiftPressed) {
+                    // Note the event
+                    hasInputBetweenShift = YES;
+                }
+                // Do not handle event because the input is english
+                handled = NO;
+                break;
+            default:
+                break;
+        }
     }
     return handled;
 }
@@ -68,6 +135,7 @@
 {
     if (self = [super initWithServer:server delegate:delegate client:inputClient]) {
         NSLog(@"Server init ok for %@", [inputClient bundleIdentifier]);
+        editors = [[NSMutableArray alloc] init];
         [self createFullpinyinEditor];
     }
 
@@ -89,6 +157,16 @@
 
     // LibpinyinFullPinyinEditor
     _fullpinyinEditor = [[LibpinyinFullPinyinEditor alloc] initWithConfig:config];
+    [editors addObject:_fullpinyinEditor];
+    // TODO: Punct
+    // [editors addObject:nil];
+    // TODO: Raw
+    // [editors addObject:nil];
+    // TODO: English
+    // [editors addObject:nil];
+
+    // Reset mode
+    mode = PINYIN_EDITOR;
 }
 
 - (NSMenu *)menu {
